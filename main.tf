d2830e5871458af585eda91f4c9c62a0aa86ea75
bk018 <bharath.koraboina018@gmail.com>
2022-05-24 13:31:16 +0530

    myec2

--- main.tf
+++ main.tf
@@ -8,8 +8,3 @@ resource "aws_instance" "myec2" {
   ami           = "ami-0022f774911c1d690"
   instance_type = "t2.nano"
 }
-
-resource "aws_eip" "lb" {
-  instance = aws_instance.myec2.id
-  vpc      = true
-}
